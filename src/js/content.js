502f5d4b00f7cca8b9f9a05d28a6fef114c570b7
Denis, Makarov <makarovdenis07081985@gmail.com>
2019-05-10 19:06:41 +0300

    login11

--- src/js/content.js
+++ src/js/content.js
@@ -4,11 +4,13 @@ let products = Data.loadProducts();
 import {Builder} from "./modules/build";
 let createCards = new Builder();
 
-import {createPag} from "./modules/render_content";
-import {cheangePag} from "./modules/render_content";
+// import {createPag} from "./modules/render_content";
+// import {cheangePag} from "./modules/render_content";
+// import {render} from "./modules/render_content";
 
 // import {RenderContent} from "./modules/render_content";
 // let renderContent = new RenderContent;
+
 
 
 let boxContent = document.getElementById("range_content_box");
@@ -19,8 +21,8 @@ export let current = 0;
 
 
 render(products);
-createPag(products, paginationBox, per_page);
-cheangePag(products, current, render(products));
+createPag(products);
+cheangePag(products);
 
 
 function firstI() {
@@ -31,35 +33,35 @@ function secondI(i, array_products) {
     return i < current * per_page + per_page && i < array_products.length;
 };
 
-/////////
-// function render(array_products) {
-//     document.getElementById("range_content_box").innerHTML = "";
-//     for (let i = firstI(); secondI(i, array_products); i++) {
-//         boxContent.appendChild(createCards.createCard(products[i]))
-//     }
-//     console.log(boxContent)
-// }
-/////////
+///////
+function render(array_products) {
+    document.getElementById("range_content_box").innerHTML = "";
+    for (let i = firstI(); secondI(i, array_products); i++) {
+        boxContent.appendChild(createCards.createCard(products[i]))
+    }
+    console.log(boxContent)
+}
+///////
 function createProducts() {
     for (let i = 0; i < products.length; i++) {
         productsBox.appendChild(createCards.createCard(products[i]))
     }
 }
-/////////
-// function createPag(prod) {
-//     for (let i = 0; i < Math.ceil(prod.length / per_page); i++) {
-//         paginationBox.appendChild(Builder.createNewElement("div", "pag_item", i + 1, [{"name": "data-name", "value": i}]));
-//     }
-// }
+///////
+function createPag(prod) {
+    for (let i = 0; i < Math.ceil(prod.length / per_page); i++) {
+        paginationBox.appendChild(Builder.createNewElement("div", "pag_item", i + 1, [{"name": "data-name", "value": i}]));
+    }
+}
 
-// function cheangePag(prod){
-//     Array.from(document.getElementsByClassName("pag_item")).map((el)=>{
-//         el.addEventListener("click", (e)=>{
-//             current = el.dataset.name;
-//             render(prod);
-//             console.log("current "+current)
-//         })
-//     })
-// }
+function cheangePag(prod){
+    Array.from(document.getElementsByClassName("pag_item")).map((el)=>{
+        el.addEventListener("click", (e)=>{
+            current = el.dataset.name;
+            render(prod);
+            console.log("current "+current)
+        })
+    })
+}
 
 
--- src/js/modules/render_content.js
+++ src/js/modules/render_content.js
@@ -1,37 +1,7 @@
-import {Builder} from "./build";
-let createCards = new Builder();
+// import {Builder} from "./build";
+// let createCards = new Builder();
+//
+// import {per_page} from "../content";
+// import {current} from "../content";
 
-
-export function createPag(prod, paginationBox, per_page) {
-    for (let i = 0; i < Math.ceil(prod.length / per_page); i++) {
-        paginationBox.appendChild(Builder.createNewElement("div", "pag_item", i + 1, [{"name": "data-name", "value": i}]));
-    }
-}
-
-export function cheangePag(prod, current){
-    Array.from(document.getElementsByClassName("pag_item")).map((el)=>{
-        el.addEventListener("click", (e)=>{
-            current = el.dataset.name;
-            render(prod);
-            console.log("current "+current)
-        })
-    })
-}
-
-export function render(array_products, boxContent) {
-    document.getElementById("range_content_box").innerHTML = "";
-    for (let i = firstI(); secondI(i, array_products); i++) {
-        boxContent.appendChild(createCards.createCard(products[i]))
-    }
-    console.log(boxContent)
-}
-
-
-function firstI() {
-    return current * per_page;
-};
-
-function secondI(i, array_products) {
-    return i < current * per_page + per_page && i < array_products.length;
-};
 
