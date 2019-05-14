075579b27bdd42f10fe7a40e37aa72effe38c50f
Denis, Makarov <makarovdenis07081985@gmail.com>
2019-05-14 12:19:10 +0300

    first logic ended

--- src/js/content.js
+++ src/js/content.js
@@ -4,116 +4,151 @@ let products = Data.loadProducts();
 import {Builder} from "./modules/build";
 let createCards = new Builder();
 
-import {sex, man, woman, children} from "./modules/render_content"; ////
+let per_page = 6;
+let current = 0;
 
-/////////
-// import {createPag} from "./modules/render_content";
-// import {renderProducts} from "./modules/render_content";
-// import {fillterProducts} from "./modules/render_content";
-// import {filterProd} from "./modules/render_content";
-// import {search} from "./modules/render_content";
-/////////
-import {renderProd} from "./modules/render_content";
-import {renderPag} from "./modules/render_content";
-import {optionSex} from "./modules/render_content";
-import {searchProd} from "./modules/render_content";
-
-
-//// => all import from render_content
 
 let boxContent = document.getElementById("range_content_box");
 let paginationBox = document.getElementById("pag_box");
 
 let changeMan = document.getElementById("buttom_man");
 let changeWoman = document.getElementById("buttom_woman");
+let changeChildren = document.getElementById("buttom_children");
+
 let changeAll = document.getElementById("buttom_all");
-// let searchForm = document.getElementById("search");
-// let btn_search = document.getElementById("btn_search")
+let sex = "sex", man = "man", woman = "woman", children = "children";
+let searchForm = document.getElementById("search");
+let btn_search = document.getElementById("btn_search");
 
 let x = [];
 let mainProd = x.concat(products);
 
 
-export let per_page = 6;
-export let current = 0;
-
-
-function shareRender(products, boxContent, paginationBox) {
-    renderProd(products, boxContent);
-    renderPag(products, paginationBox);
-    cheangePag(products);
+function shareRender(mainProd) {
+    renderProd(mainProd);
+    renderPag(mainProd);
+    cheangePag(mainProd);
+    searchProd(mainProd);
+    setTimeout(e => showEmount(),10)
 }
 
+shareRender(mainProd);
 
 
-shareRender(mainProd, boxContent, paginationBox);
-searchProd(mainProd, boxContent, paginationBox, current);
+///////     FUNCTIONS
+function firstI() {
+    return current * per_page;
+};
+
+function secondI(i, prod) {
+    return i < current * per_page + per_page && i < prod.length;
+};
 
 
+function renderProd(prod) {
+    boxContent.innerHTML = "";
+    for (let i = firstI(); secondI(i, prod); i++) {
+        boxContent.appendChild(createCards.createCard(prod[i]))
+    }
+}
 
-///////
-// function searchProd(prod, paginationBox) {
-//     let btn_search = document.getElementById("btn_search");
-//     let searchForm = document.getElementById("search");
-//     current = 0;
-//
-//     let searchProd;
-//
-//     btn_search.addEventListener("click", (e)=>{
-//         if (searchForm.value == ""){
-//             console.log("nothing")
-//         } else{
-//             paginationBox.innerHTML ="";
-//             console.log(prod);
-//             let searchRegExp = new RegExp(searchForm.value, "i");
-//             let x;
-//             x = prod.filter(el => searchRegExp.test(el.name));
-//             //
-//             console.log(x);
-//             renderProd(x);
-//             renderPag(x, paginationBox);
-//         }
-//     })
-// }
-///////
-export function cheangePag(prod){
-    Array.from(document.getElementsByClassName("pag_item")).map((el)=>{
-        el.addEventListener("click", (e)=>{
+function renderPag(prod) {
+    paginationBox.innerHTML = "";
+    for (let i = 0; i < Math.ceil(prod.length / per_page); i++) {
+        paginationBox.appendChild(Builder.createNewElement("div", "pag_item", i + 1, [{
+            "name": "data-name",
+            "value": i
+        }]));
+    }
+}
+
+function cheangePag(prod) {
+    Array.from(document.getElementsByClassName("pag_item")).map((el) => {
+        el.addEventListener("click", (e) => {
             current = el.dataset.name;
-            renderProd(prod);    // from modules
-            console.log("current "+current)
-            searchProd(prod, boxContent, paginationBox);
+            renderProd(prod);
+            console.log("current " + current);
+            // searchProd(prod, paginationBox);
         })
     })
 }
 
-changeMan.addEventListener("click",(e)=>{
-    current =0; mainProd = optionSex(products, man);  // from modules
-    shareRender(mainProd, boxContent, paginationBox);
-    searchProd(mainProd, boxContent, paginationBox)
+function optionSex(prod, sexTarget) {
+    let filterProd = prod.filter(el => el.sex == sexTarget);
+    return filterProd
+}
+
+
+changeMan.addEventListener("click", (e) => {
+    current = 0;
+    mainProd = optionSex(products, man);
+    shareRender(mainProd);
+    searchProd(mainProd);
     return mainProd
 });
 
-changeWoman.addEventListener("click",(e)=>{
-    current =0; mainProd = optionSex(products, woman);  // from modules
-    shareRender(mainProd, boxContent, paginationBox);
-    searchProd(mainProd, boxContent, paginationBox)
+changeWoman.addEventListener("click", (e) => {
+    current =0;
+    mainProd = optionSex(products, woman);  // from modules
+    shareRender(mainProd);
+    searchProd(mainProd);
+    return mainProd
+});
+changeChildren.addEventListener("click", (e) => {
+    current =0;
+    mainProd = optionSex(products, children);  // from modules
+    shareRender(mainProd);
+    searchProd(mainProd);
     return mainProd
 });
 
-changeAll.addEventListener("click",(e)=>{
+changeAll.addEventListener("click", (e) => {
     // paginationBox.innerHTML ="";
     mainProd = products;
-    shareRender(mainProd, boxContent, paginationBox);
-    searchProd(mainProd, boxContent, paginationBox)
+    shareRender(mainProd);
+    searchProd(mainProd);
     console.log(mainProd)
 });
 
-let buttom_test = document.getElementById("buttom_test")
-    buttom_test.addEventListener("click", (e)=>{
-        console.log(mainProd);
-        console.log(current)
-    })
 
-// let x = filterProd(products, sex, man)
-// console.log(x)
+function search(prod) {
+    current = 0; paginationBox.innerHTML = "";
+    let searchRegExp = new RegExp(searchForm.value, "i");
+    prod = prod.filter(el => searchRegExp.test(el.name));
+    shareRender(prod);
+    return mainProd = prod;
+}
+
+function searchProd(prod) {
+    let searchProd;
+
+    btn_search.addEventListener("click", (e) => {
+        if (searchForm.value == "") {
+            // boxContent.innerHTML ="hDJLHSDJKhLHJKD"
+        } else {
+            search(prod)
+        }
+    });
+
+    searchForm.addEventListener("keypress", (e)=>{
+        if(e.keyCode == 13){
+            search(prod)
+        }
+    });
+}
+
+
+
+
+function showEmount() {
+    document.getElementById("amountItem").innerHTML = "";
+    document.getElementById("amountItem").innerHTML = "Amount " + mainProd.length
+}
+
+/////////////////////////////
+
+let buttom_test = document.getElementById("buttom_test")
+buttom_test.addEventListener("click", (e) => {
+    console.log(mainProd);
+    console.log(current)
+})
--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -5,17 +5,24 @@ const load_products = [
     {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "man", "images": "7_shoe_item_1.png"},
     {"name":"Asics", "price": 150.50, "priceDown": 35.00, "boxSale": 20, "sex": "man", "images": "7_shoe_item_1.png"},
     {"name":"Nike", "price": 100.50, "priceDown": 45.00, "boxSale": 25, "sex": "man", "images": "7_shoe_item_1.png"},
-    {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "man", "images": "7_shoe_item_1.png"},
+    {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "children", "images": "7_shoe_item_1.png"},
     {"name":"Reebok", "price": 150.50, "priceDown": 35.00, "boxSale": 20, "sex": "man", "images": "7_shoe_item_1.png"},
-    {"name":"Nike", "price": 100.50, "priceDown": 45.00, "boxSale": 25, "sex": "man", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 577.50, "priceDown": 45.00, "boxSale": 25, "sex": "children", "images": "7_shoe_item_1.png"},
     {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "man", "images": "7_shoe_item_1.png"},
     {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "woman", "images": "7_shoe_item_1.png"},
-
     {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "woman", "images": "7_shoe_item_1.png"},
     {"name":"Asics", "price": 150.50, "priceDown": 35.00, "boxSale": 20, "sex": "woman", "images": "7_shoe_item_1.png"},
     {"name":"Nike", "price": 100.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"},
     {"name":"Reebok", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "woman", "images": "7_shoe_item_1.png"},
     {"name":"Adidas", "price": 180.50, "priceDown": 25.00, "boxSale": 15, "sex": "woman", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 100.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 123.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 33.50, "priceDown": 45.00, "boxSale": 25, "sex": "children", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 345.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 234.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 57.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"},
+    {"name":"Nike", "price": 57.50, "priceDown": 45.00, "boxSale": 25, "sex": "woman", "images": "7_shoe_item_1.png"}
+
 
 ];
 
@@ -31,4 +38,5 @@ export class Data {
     static getProducts() {
         return products;
     }
+
 }
